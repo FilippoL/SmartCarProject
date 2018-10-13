35f4e81c24e6283a20534801b142e0c0323af9bb
Filippo Libardi <pippo.libardi@gmail.com>
2018-10-13 20:44:46 +0100

    Created Camera class for less messy setup

--- CameraManager.py
+++ CameraManager.py
@@ -3,30 +3,36 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
- 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
- 
-# allow the camera to warmup
-time.sleep(0.1)
- 
+
+class Camera:
+	def __init__(self, resolution =  (640, 480), framerate = 32):
+		# initialize the camera and grab a reference to the raw camera capture
+		self.camera = PiCamera()
+		self.resolution = resolution
+		self.framerate = framerate
+
+
+ 	def initialize(self):
+
+		rawCapture = PiRGBArray(self.camera, size=(640, 480))
+		time.sleep(0.1)
+
+	def start(self):
+
+
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	# grab the raw NumPy array representing the image, then initialize the timestamp
 	# and occupied/unoccupied text
 	image = frame.array
- 
+
 	# show the frame
 	cv2.imshow("Frame", image)
 	key = cv2.waitKey(1) & 0xFF
- 
+
 	# clear the stream in preparation for the next frame
 	rawCapture.truncate(0)
- 
+
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
-
